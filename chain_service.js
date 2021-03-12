3264770ad4b6f2be99581d7fe52a98b76685620a
Cristina Manrique <cmanrique@catalyst.com.mx>
2021-03-12 00:19:14 -0600

    Add endpoitns to send and check BNB

--- chain_service.js
+++ chain_service.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const { check, validationResult, param } = require('express-validator')
-const config = require('./config/config')
-const chain = require ('./chain')
-const getKeys = require('./kms')
-const RemittancesPoller = require("./scheduled_remittances_polling");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { check, validationResult, param } = require('express-validator');
+const config = require('./config/config');
+const chain = require('./chain');
+const getKeys = require('./kms');
+const RemittancesPoller = require('./scheduled_remittances_polling');
 const port = config.port;
 const chainName = config.chain;
 
@@ -23,80 +23,114 @@ poller.poll();
 const middleware = [
   bodyParser.json(),
   bodyParser.urlencoded({ extended: true }),
-  check()
-]
+  check(),
+];
 
-app.use(middleware)
+app.use(middleware);
 
 app.get('/', (req, res) => {
-  res.send('live')
-})
+  res.send('live');
+});
 
 app.get('/create_account', async (req, res) => {
-  try{
-    const errors = validationResult(req)
-    if(!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() })
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
     //res.setTimeout(3000)
-    let datos = await chain.createAccount()
-    return res.status(200).send(datos)
-
-  }catch(error){
-    return res.status(500).send(error.message)
+    let datos = await chain.createAccount();
+    return res.status(200).send(datos);
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
-})
+});
 
 const sendCheck = [
   check('sender_kms_key').exists().trim().escape(),
   check('receiver_kms_key').exists().trim().escape(),
   check('amount').exists().trim().escape(),
-  check('asset').exists().trim().escape()
-]
+  check('asset').exists().trim().escape(),
+];
 
 app.post('/send', sendCheck, async (req, res) => {
-  try{
-    const errors = validationResult(req)
-    if(!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() })
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
     //res.setTimeout(3000)
     const { amount, asset, receiver_kms_key, sender_kms_key } = req.body;
     const kmsKeys = await getKeys(sender_kms_key.toString());
     const receiverKeys = await getKeys(receiver_kms_key.toString());
-    const secret = kmsKeys["keys"][chainName]["private_key"];
+    const secret = kmsKeys['keys'][chainName]['private_key'];
     const to =
-      receiverKeys["keys"][chainName]["public_key"] ||
-      receiverKeys["keys"][chainName]["address"];
+      receiverKeys['keys'][chainName]['public_key'] ||
+      receiverKeys['keys'][chainName]['address'];
     const from =
-      kmsKeys["keys"][chainName]["public_key"] ||
-      kmsKeys["keys"][chainName]["address"];
+      kmsKeys['keys'][chainName]['public_key'] ||
+      kmsKeys['keys'][chainName]['address'];
 
     await chain.send(to, amount, asset, secret, from);
-    return res.status(200).send("Send finished!");
-
-  }catch(error){
-    return res.status(500).send(error.message)
+    return res.status(200).send('Send finished!');
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
-})
+});
+
+const gasCheck = [
+  check('fromKey').exists().trim().escape(),
+  check('to').exists().trim().escape(),
+];
+
+app.post('/gas', gasCheck, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
+
+    const { fromKey, to } = req.body;
+    await chain.gas(fromKey, to);
+    return res.status(200).send('Send gas finished!');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+const balanceCheck = [check('address').exists().trim().escape()];
+
+app.get('/balance/:address', balanceCheck, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(422).json({ errors: errors.array() });
+
+  try {
+    const { address } = req.params;
+    const balance = await chain.balance(address);
+    console.log(balance);
+    return res.status(200).send({ balance: balance });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 
 app.get('/fees/:chain', async (req, res) => {
   const _chainName = req.params.chain;
-  
+
   // Validate params
   const isStellar = _chainName === 'stellar';
   const isBinance = _chainName === 'binance';
-  
-  if(isStellar || isBinance){
-    try {      
-      const networkFees = await chain.fee();      
-      res.status(200).send({ airprotocolFee:0.001, _chainName, networkFees })
+
+  if (isStellar || isBinance) {
+    try {
+      const networkFees = await chain.fee();
+      res.status(200).send({ airprotocolFee: 0.001, _chainName, networkFees });
     } catch (e) {
       res.status(500).send(e.message);
     }
   } else {
     res.status(400).send('Chain not found');
   }
-})
+});
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
